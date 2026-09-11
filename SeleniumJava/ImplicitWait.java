588ee2009985a09f1d9fd56cdb194cd8c29cf6f6
ShubhangiPatil01 <spshreegajanan@gmail.com>
2026-09-11 17:23:27 +0530

    Add files via upload

--- SeleniumJava/ImplicitWait.java
+++ SeleniumJava/ImplicitWait.java
@@ -1,1 +1,27 @@
+package TestDemo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ImplicitWait {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+		
+        // Implicit wait of 10 seconds
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.facebook.com/login.php/");
+
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("Test@gmail.com");
+		driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("12345678");
+		driver.findElement(By.xpath("//span[text()='Log in']")).click();
+       
+        driver.quit();
+    }
+}
